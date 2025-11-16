d8c49a130551657b17db1108afae3f5a0460f187
Jack Harrington <github@skipmail.sentienthouseplant.com>
2025-11-16 15:30:39 +0000

    Add `hello.py` script.

---
+++ embeddings/hello.py
@@ -0,0 +1,12 @@
+# /// script
+# requires-python = ">=3.10"
+# dependencies = [
+#     "rich"
+# ]
+# ///
+
+from rich.console import Console
+
+console = Console()
+
+console.print("Hello, world!")
